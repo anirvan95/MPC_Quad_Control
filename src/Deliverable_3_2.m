37b477c1fef0f5bc438489dde8fa5e521b6eeb1b
Travis CI <travis@travis-ci.org>
2020-01-02 13:50:41 +0100

    Added plots and zip files for 3.2

---
+++ src/Deliverable_3_2.m
@@ -0,0 +1,39 @@
+clear all
+clc
+
+Ts = 1/5;
+quad = Quad(Ts);
+[xs, us] = quad.trim();
+sys = quad.linearize(xs, us);
+[sys_x, sys_y, sys_z, sys_yaw] = quad.decompose(sys, xs, us);
+
+mpc_x = MPC_Control_x(sys_x, Ts);
+mpc_y = MPC_Control_y(sys_y, Ts);
+mpc_z = MPC_Control_z(sys_z, Ts);
+mpc_yaw = MPC_Control_yaw(sys_yaw, Ts);%% Restart
+
+
+ref_x = @(t, x) [-2; 0; 0; 0];
+ref_y = @(t, x) [0; -2; 0; 0];
+ref_z = @(t, x) [0; 0; -2; 0];
+ref_yaw = @(t, x) [0; 0; 0; pi/4];
+%% plt-X
+sim = quad.sim(mpc_x, mpc_y, mpc_z, mpc_yaw, 0, ref_x);
+quad.plot(sim);
+
+%% plt-Y
+sim = quad.sim(mpc_x, mpc_y, mpc_z, mpc_yaw, 0, ref_y);
+quad.plot(sim);
+
+%% plt-Z
+sim = quad.sim(mpc_x, mpc_y, mpc_z, mpc_yaw, 0, ref_z);
+quad.plot(sim);
+
+%% plt-yaw
+sim = quad.sim(mpc_x, mpc_y, mpc_z, mpc_yaw, 0, ref_yaw);
+quad.plot(sim);
+
+%% plt-ref
+ref = @(t, x) [2; 2; 2; 0];
+sim = quad.sim(mpc_x, mpc_y, mpc_z, mpc_yaw, 0, ref);
+quad.plot(sim);
